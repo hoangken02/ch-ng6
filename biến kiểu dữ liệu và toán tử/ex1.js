b3de4b9fcf1bdf867b5893d50a9b95d3917419de
- <=>
2020-10-12 08:40:50 +0700

    chuong7

---
+++ biến kiểu dữ liệu và toán tử/ex1.js
@@ -0,0 +1,14 @@
+let inputpysics;
+let inputchemistry;
+let inputbiology;
+inputpysics = prompt("điểm lý");
+inputchemistry = prompt("điểm hóa");
+inputbiology = prompt("điểm sinh");
+let P = + inputpysics;
+let C = + inputchemistry;
+let B = + inputbiology;
+let S = P + C +B;
+document.write("tổng là: " +S);
+let A =(P + C + B)/3;
+document.write("trung bình 3 môn là: " +A)
+
---
+++ sử dụng toán tử/operato.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputheight;
+inputWidth = prompt("nhập chiều cao");
+inputheight = prompt("nhập chiều rộng");
+let width = + parseInt(inputWidth);
+let height = + parseInt(inputheight);
+let area = width * height;
+document.write("diện tích là: " +area)
